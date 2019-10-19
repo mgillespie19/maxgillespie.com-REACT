b21a7439f714e3b2f99a1ca47080c8b017c8189c
Max Gilespie <max.gillespie10@gmail.com>
2019-10-18 22:38:30 -0500

    add header, header styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div className="Landing">
     <div className="Summary">
-      <h2> developer </h2>
-      <h2> ui/ux designer </h2>
-      <h2> filmmaker </h2>
-      <h2> long-walk-on-the-beach taker </h2>
+      <h2> about! </h2>
     </div>
   </div>
 )
