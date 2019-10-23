0ee0ba1619d3d283d02e719cbfeb7607f960b52d
Max Gilespie <max.gillespie10@gmail.com>
2019-10-23 14:38:08 -0500

    landing styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,20 @@ import rightHead from '../assets/head-right.png'
 import leftHead from '../assets/head-left.png'
 
 const IndexPage = () => (
-  <div  className="Landing">
-    <div id="rightHead">
-      <img src={rightHead} />
-    </div>
-    <div className="Summary">
-      <h1> developer </h1>
-      <h1> ui/ux designer </h1>
-      <h1> filmmaker </h1>
-      <h1> long-walk-on-the-beach taker </h1>
-    </div>
-    <div id="leftHead">
-      <img src={leftHead} />
+  <div className="Page">
+    <div  className="Landing">
+      <div id="rightHead">
+        <img src={rightHead} />
+      </div>
+      <div className="Summary">
+        <h1> developer </h1>
+        <h1> ui/ux designer </h1>
+        <h1> filmmaker </h1>
+        <h1> long-walk-on-the-beach taker </h1>
+      </div>
+      <div id="leftHead">
+        <img src={leftHead} />
+      </div>
     </div>
   </div>
 )
