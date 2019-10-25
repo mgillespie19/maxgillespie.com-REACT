aaa5a4a8b69d1ae13cf9c6d18eacb1e4dc361d78
Max Gilespie <max.gillespie10@gmail.com>
2019-10-25 14:20:17 -0500

    working on macbook

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,6 @@ module.exports = {
     title: 'Max Gillespie'
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'GatsbyJS',
-        short_name: 'GatsbyJS',
-        start_url: '/',
-        background_color: '#f7f0eb',
-        theme_color: '#a2466c',
-        display: 'standalone',
-        icon: 'src/assets/my-icon.ico',
-      },
-    },
+    'gatsby-plugin-react-helmet'
   ],
 }
