fcfbec28cfa3a2aa05885cb204386d13a4068e30
Max Gilespie <max.gillespie10@gmail.com>
2019-10-25 20:06:22 -0500

    add columns + variable to reusable component

--- src/components/exampleCard.js
+++ src/components/exampleCard.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './exampleCard.css'
 
+
+
 const ExampleCard = ({ siteTitle, projectTitle, imageLink, description_p1,
-                        description_p2, destinationPage }) => (
+                        description_p2, destinationPage, imgSrc }) => (
   <div>
     <div className="Example">
+      <div className="Logo">
+        <img src={imgSrc} />
+      </div>
       <div className="InnerContent">
         <h1> {projectTitle} </h1>
         <p> {description_p1} </p>
