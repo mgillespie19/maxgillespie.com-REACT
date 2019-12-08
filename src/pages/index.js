7fa4ef863cc9750f4ea0d3983bcdb98a1880393d
Max Gilespie <max.gillespie10@gmail.com>
2019-12-07 22:02:22 -0600

    page color changes for tiles

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,89 +31,101 @@ const IndexPage = () => (
       </div>
     </div>
 
-    <div className="QuickPreamble">
-      <h1>
-        my work
-      </h1>
-      <p>
-        this is a collection of my work -- some of the things I'm passionate about,
-        proud to have worked on, or am otherwise excited to share.
-      </p>
-      <p>
-        if you like what you see, and are interested in working together on a project,
-        check out <Link to="/about"> <b> contact </b> </Link> in my about section,
-        or head over to <Link to="/"> <b> my contracting company </b> </Link>
-      </p>
+    <div className="QuickPreambleHeader">
+
     </div>
 
-    <div className="WorkRowOne">
-      <ExampleSquare
-        projectTitle="Maven Machines"
-        description_p1="My job as an Android Development Intern at a startup in Pittsburgh"
-        destinationPage="/mavenMachines"
-        imgSrc={mavenImg}
-      />
+    <div className="BottomHalf">
+      <div className="QuickPreambleBody">
+        <h1>
+          my work
+        </h1>
+        <p>
+          this is a collection of my work -- some of the things I'm passionate about,
+          proud to have worked on, or am otherwise excited to share.
+        </p>
+        <p>
+          if you like what you see, and are interested in working together on a project,
+          check out <Link to="/about"> <b> contact </b> </Link> in my about section,
+          or head over to <Link to="/"> <b> my contracting company </b> </Link>
+        </p>
+      </div>
 
-      <ExampleSquare
-        projectTitle="Grid"
-        description_p1="An iOS app I made for connecting groups to people"
-        destinationPage="/grid"
-        imgSrc={gridImg}
-      />
+      <div className="WorkRowOne">
+        <ExampleSquare
+          projectTitle="Maven Machines"
+          description_p1="My job as an Android Development Intern at a startup in Pittsburgh"
+          destinationPage="/mavenMachines"
+          imgSrc={mavenImg}
+        />
 
-      <ExampleSquare
-        projectTitle="myEHR"
-        description_p1="A class project using blockchain to help the healthcare system"
-        destinationPage="/myEHR"
-        imgSrc={myEHRImg}
-      />
+        <ExampleSquare
+          projectTitle="Grid"
+          description_p1="An iOS app I made for connecting groups to people"
+          destinationPage="/grid"
+          imgSrc={gridImg}
+        />
 
-      <ExampleSquare
-        projectTitle="Personal Work"
-        description_p1="Things I make in my free time that I'm passionate about
-                        and want to share"
-        destinationPage="/personalWork"
-        imgSrc={personalWorkImg}
-      />
+        <ExampleSquare
+          projectTitle="myEHR"
+          description_p1="A class project using blockchain to help the healthcare system"
+          destinationPage="/myEHR"
+          imgSrc={myEHRImg}
+        />
+
+        <ExampleSquare
+          projectTitle="Personal Work"
+          description_p1="Things I make in my free time that I'm passionate about
+                          and want to share"
+          destinationPage="/personalWork"
+          imgSrc={personalWorkImg}
+        />
+      </div>
+
+
+      <div className="WorkRowTwo">
+        <ExampleSquare
+          projectTitle="Small Projects"
+          description_p1="Software projects that I've worked on (for the most part
+                      exclusively) which showcase smaller skills, but aren't
+                      full applications"
+          destinationPage="/smallProjects"
+          imgSrc={testflightImg}
+        />
+
+        <ExampleSquare
+          projectTitle="myEHR"
+          description_p1="A class project from my junior year of college exploring
+                          the potential of blockchain in helping the healthcare system"
+          destinationPage="/myEHR"
+          imgSrc={myEHRImg}
+        />
+
+        <ExampleSquare
+          projectTitle="Personal Work"
+          description_p1="Things I make in my free time that I'm passionate about
+                          and want to share"
+          destinationPage="/personalWork"
+          imgSrc={personalWorkImg}
+        />
+
+        <ExampleSquare
+          projectTitle="Small Projects"
+          description_p1="Software projects that I've worked on (for the most part
+                      exclusively) which showcase smaller skills, but aren't
+                      full applications"
+          destinationPage="/smallProjects"
+          imgSrc={testflightImg}
+        />
+      </div>
     </div>
 
-
-    <div className="WorkRowTwo">
-      <ExampleSquare
-        projectTitle="Small Projects"
-        description_p1="Software projects that I've worked on (for the most part
-                    exclusively) which showcase smaller skills, but aren't
-                    full applications"
-        destinationPage="/smallProjects"
-        imgSrc={testflightImg}
-      />
-
-      <ExampleSquare
-        projectTitle="myEHR"
-        description_p1="A class project from my junior year of college exploring
-                        the potential of blockchain in helping the healthcare system"
-        destinationPage="/myEHR"
-        imgSrc={myEHRImg}
-      />
-
-      <ExampleSquare
-        projectTitle="Personal Work"
-        description_p1="Things I make in my free time that I'm passionate about
-                        and want to share"
-        destinationPage="/personalWork"
-        imgSrc={personalWorkImg}
-      />
-
-      <ExampleSquare
-        projectTitle="Small Projects"
-        description_p1="Software projects that I've worked on (for the most part
-                    exclusively) which showcase smaller skills, but aren't
-                    full applications"
-        destinationPage="/smallProjects"
-        imgSrc={testflightImg}
-      />
+    <div className="Footer">
+      <p> © 2019 max gillespie </p>
+      <p> 6105 Delmar Blvd </p>
+      <p> St. Louis, MO 63112</p>
+      <p> inquiries: max@maxgillespie.com </p>
     </div>
-
   </div>
 )
 
