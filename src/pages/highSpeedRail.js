a2418ce0ba18245561121e731b87e760752e3584
Max Gilespie <max.gillespie10@gmail.com>
2020-01-02 10:24:47 -0700

    add pages for magazine example 310

--- src/pages/highSpeedRail.js
+++ src/pages/highSpeedRail.js
@@ -2,6 +2,18 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import '../layouts/workLayout.css'
+import '../layouts/highSpeedRail.css'
+import p1 from '../assets/FutureOfTransport/FutureOfTransport_p1.png'
+import p2 from '../assets/FutureOfTransport/FutureOfTransport_p2.png'
+import p3 from '../assets/FutureOfTransport/FutureOfTransport_p3.png'
+import p4 from '../assets/FutureOfTransport/FutureOfTransport_p4.png'
+import p5 from '../assets/FutureOfTransport/FutureOfTransport_p5.png'
+import p6 from '../assets/FutureOfTransport/FutureOfTransport_p6.png'
+import p7 from '../assets/FutureOfTransport/FutureOfTransport_p7.png'
+import p8 from '../assets/FutureOfTransport/FutureOfTransport_p8.png'
+import p9 from '../assets/FutureOfTransport/FutureOfTransport_p9.png'
+import p10 from '../assets/FutureOfTransport/FutureOfTransport_p10.png'
+import p11 from '../assets/FutureOfTransport/FutureOfTransport_p11.png'
 
 const IndexPage = () => (
   <div className="WorkDetail">
@@ -21,9 +33,9 @@ const IndexPage = () => (
 
     <h2> what is it? </h2>
     <p>
-      I decided to make a mock magazine article for a magazine called
-      Popular Mechanics -- I wrote about the future of high speed rail, and
-      green transportation at large, in America and the roadblocks we will face
+      I made a mock magazine article for a magazine called Popular Mechanics
+       -- I wrote about the future of high speed rail and green transportation
+      in America, as well as the roadblocks we will face
       along the way in the realization of these technologies.
     </p>
 
@@ -36,6 +48,20 @@ const IndexPage = () => (
         Adobe Illustrator
       </li>
     </ul>
+
+    <div className="futureOfTransportMagazinePages">
+      <img className="page" src={p1} />
+      <img className="page" src={p2} />
+      <img className="page" src={p3} />
+      <img className="page" src={p4} />
+      <img className="page" src={p5} />
+      <img className="page" src={p6} />
+      <img className="page" src={p7} />
+      <img className="page" src={p8} />
+      <img className="page" src={p9} />
+      <img className="page" src={p10} />
+      <img className="page" src={p11} />
+    </div>
 
   </div>
 )
