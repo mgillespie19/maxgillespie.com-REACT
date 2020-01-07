2cf8decc68e9c459800df2b342be0891bbe9ae6c
Max Gilespie <max.gillespie10@gmail.com>
2020-01-06 17:14:47 -0700

    add mountain graphic

--- src/pages/personalWork.js
+++ src/pages/personalWork.js
@@ -2,10 +2,20 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import '../layouts/workLayout.css'
+import '../layouts/personalWorkLayout.css'
+
+import mountain from '../assets/Personal_Projects/mountain.png'
 
 const IndexPage = () => (
   <div className="WorkDetail">
     <h1> Personal Work </h1>
+
+    <div className="personalWorkImg">
+      <h3> the man who moves a mountain </h3>
+      <img src={mountain} />
+      <p> Photo by Joshua Earle on Unsplash </p>
+    </div>
+
   </div>
 )
 
