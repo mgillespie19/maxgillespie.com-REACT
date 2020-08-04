fd1cb6539b2128b46ac602a8b733df81f7aec9bb
Max Gilespie <max.gillespie10@gmail.com>
2020-08-03 21:09:46 -0400

    add bullet points to maven

--- src/pages/mavenMachinesFullTime.js
+++ src/pages/mavenMachinesFullTime.js
@@ -14,38 +14,45 @@ const IndexPage = () => (
     <h1> Maven Machines </h1>
 
     <p>
-    Maven is a startup in Pittsburgh, Pennsylvania that
+    Maven is a Series A ($8.3M total) B2B SaaS startup in Pittsburgh, Pennsylvania that
     makes asset tracking software for the transportation industry.
     I worked with Maven during the summer of 2019 as an Android
-    Devloper.
-    </p>
-    <p>
-    I maintained and built new features within the
-    codebase for their app that services thousands of truck
-    drivers and dispatchers daily."
+    Devloper, and now work full-time at the company as a product 
+    designer and software engineer.
     </p>
 
     <div className='mavenDetailImages1'>
       <img src={tabletImage} />
     </div>
 
-    <h2> RESPONSIBILITIES </h2>
+    <h2> WHAT I DO </h2>
     <ul>
       <li>
-        Worked in an agile development environment on the Android Application team
-      </li>
-      <li> Wrote scalable software in Kotlin and Java </li>
-      <li>
-        Built multiple new product features that were released to thousands
-        of drivers across multiple fleets
+      Key contributor in the design and development of ~350 deliverables over a 3 month window for a key customer
       </li>
       <li>
-        Part of team that honed release strategy that catered to multiple
-        enterprise-level clients
+      Lead customer discover, UX journey iteration, and UI design for features critical to business development 
       </li>
       <li>
-        Improved overall ability to function as mission-critical software
-        through bug fixes and UI/UX improvements
+      Interface between the design and engineering teams to communicate goals and objectives to key stakeholders
+      </li>
+      <li>
+      Lead multiple software releases to the Google Play Store
+      </li>
+      <li>
+      Own and lead the designs, architectures, development, testing, and support of core services and infrastructure for Maven’s mobile app & backend systems
+      </li>
+      <li>
+      Work closely with product managers, designers, and engineers to create detailed and clear requirements and software designs.
+      </li>
+      <li>
+      Define, create, and execute larger technology visions within team to align with strategic business and engineering goals
+      </li>
+      <li>
+      Build tools, libraries, and write documentation to elevate your team to work as efficiently and cohesively as possible
+      </li>
+      <li>
+      Design, architect, and build world-class, scalable, performant distributed systems
       </li>
     </ul>
 
